34e9c7e58569dd04a8562b76b33e225043e3e90e
Michael <Michaellaloush@hotmail.com>
2020-05-14 05:00:10 +0300

    fixing-curropted-files

---
+++ src/main.js
@@ -0,0 +1,147 @@
+﻿
+import UIHandler from './modules/UIHandler.js'
+import DataHandler from './modules/DataHandler.js'
+
+$(async () => {
+
+    const setListHandler = new DataHandler('list');
+    let pagesContainer = await setListHandler.init();
+    let setUIHandler = new UIHandler(pagesContainer);
+
+    $(document).on('click', 'a', (evt) => {
+        evt.preventDefault()
+
+        let target = $(evt.target).attr('href');
+        const currentPage = $('.page');
+        const setAnchorHandler = new UIHandler(pagesContainer);
+
+
+        switch (target) {
+            case 'home':
+                $('.parallax').fadeIn();
+                setAnchorHandler.switchSection();
+                $('body').children('span').remove()
+                break;
+            case 'live-reports':
+                $('.parallax').fadeOut('fast');
+                setAnchorHandler.switchSection('liveReports');
+                break;
+            case 'about':
+                $('body').children('span').remove()
+                $('.parallax').fadeOut('fast')
+                setAnchorHandler.switchSection('about');
+                break;
+            case 'first-page':
+                setAnchorHandler.moveCoinPage(currentPage, 0);
+                break;
+            case 'prev-page':
+                target = currentPage.data('num') - 1
+                if (target >= 0) {
+                    setAnchorHandler.moveCoinPage(currentPage, target);
+                }
+                break;
+            case 'next-page':
+                target = currentPage.data('num') + 1
+                if (target < pagesContainer.children().length) {
+                    setAnchorHandler.moveCoinPage(currentPage, target);
+                }
+
+                break;
+            case 'last-page':
+                target = pagesContainer.children().length - 1
+                setAnchorHandler.moveCoinPage(currentPage, target);
+                break;
+        }
+
+    })
+
+    $(document).on('click', '#search-btn', async (evt) => {
+
+        evt.preventDefault();
+
+        if ($('.modal[data-belongs="search"]').length == 0) {
+            setUIHandler = new UIHandler(pagesContainer);
+            setUIHandler.searchCoin();
+        }
+    })
+
+    $(document).on('click', '.btn-close', (evt) => {
+        const target = $(evt.target).parents('.modal').data('belongs');
+        setUIHandler = new UIHandler(pagesContainer);
+        setUIHandler.closeModal(target);
+
+    })
+
+    $(document).on('click', '.coins-toggler', (evt) => {
+
+        const targetedToggle = evt.target;
+        const toggledArray = $.grep(pagesContainer.find('.coins-toggler'), toggle => toggle.checked);
+
+        setUIHandler = new UIHandler(pagesContainer);
+        if (targetedToggle.checked && toggledArray.length < 5) {
+            setUIHandler.toggleSwitched(targetedToggle, true);
+        }
+        else if (targetedToggle.checked && toggledArray.length == 5 && $('.modal[data-belongs="limit"]').length == 0) {
+            targetedToggle.checked = false
+
+            if ($('.modal[data-belongs="search"]').length == 1) {
+                setUIHandler.closeModal('search');
+            }
+            setUIHandler.toggleLimit(toggledArray, targetedToggle);
+            alert('You have reached the limits of toggable coins')
+        }
+        else {
+            setUIHandler.toggleSwitched(targetedToggle, false);
+            targetedToggle.checked = false;
+
+        }
+
+    })
+
+    $(document).on('click', '.more-info-btn', async (evt) => {
+
+        const storedData = JSON.parse(localStorage.getItem('coinsData'));
+        const coin = $(evt.currentTarget).parents('.card');
+        const coinID = $(evt.currentTarget).parents('.card').data('id');
+        const currenTime = $.now();
+
+        const coinHandler = new DataHandler(coinID);
+        setUIHandler = new UIHandler(pagesContainer);
+
+        if (storedData === null) {
+            localStorage.setItem('coinsData', JSON.stringify([]));
+            await coinHandler.updateMarketData();;
+            await coinHandler.updateCoin(coin);;
+        }
+        else {
+            const storedCoin = $.grep(storedData, coinData => coinData.coinID === coinID);
+            if (storedCoin.length === 0) {
+                await coinHandler.updateMarketData();
+                await coinHandler.updateCoin(coin);
+
+            }
+
+            else if (currenTime - storedCoin[0].last_update > 120 * 1000) {
+                await coinHandler.updateMarketData();
+                await coinHandler.updateCoin(coin);
+            }
+        }
+        setUIHandler.coinInfo(coin);
+        coin.find('.info-container').slideToggle('ease');
+                                                                                            
+    })
+
+    $(document).on('click', '#show-all', (evt) => {
+        setUIHandler = new UIHandler(pagesContainer)
+        const toggledArray = $.grep(pagesContainer.find('.coins-toggler'), toggle => toggle.checked);
+        if (toggledArray.length == 0) {
+            $('#show-all')[0].checked = false;
+            alert('there are NO selected coins, Please select some coins with the toggle');
+        }
+        else {
+            setUIHandler.toggleLimit(toggledArray);
+        }
+    })
+
+})
+
---
+++ src/modules/DataHandler.js
@@ -0,0 +1,177 @@
+﻿import { card,pagination }  from './Templates.js'
+
+
+let ticker;
+const progressBar = () => {
+    let counter = 0;
+    $('.loader').fadeIn('ease')
+    ticker = setInterval(() => {
+        counter = counter + 1;
+        if (counter <= 100) {
+            $('.loader').css('width', `${counter}%`)
+        }
+        if (counter == 100) {
+            counter = 0;
+        }
+    }, 10)
+}
+
+$(document).ajaxStart(() => {
+    const trigger = setTimeout(() => {
+        progressBar();
+    }, 500);
+    $(document).ajaxStop(() => {
+        clearTimeout(trigger);
+    })
+})
+
+function apiRequest(request) {
+    let response;
+    if (request != undefined) { 
+        if (typeof request != 'object') {
+
+            response = $.get({ url: 'https://api.coingecko.com/api/v3/coins/' + request, dataType: 'json' });
+
+
+        }    
+        else {
+            response = $.get({ url: ' https://min-api.cryptocompare.com/data/pricemulti?fsyms=' + request + '&tsyms=USD,EUR,ILS', dataType: 'json' });
+
+        }
+    }
+    return response;
+};
+
+export default class DataHandler {
+    constructor(request) {
+
+        this.request = request;
+
+    }
+
+    async init() {
+
+        const apiListData = await apiRequest(this.request);
+        const pagesContainer = $(document.createElement('div'));
+        let pagesCounter = 0;
+        //Created an interval to let the call stack event loop 'breath'
+        const dataFlowValve = setInterval(() => {
+
+            const pageData = apiListData.splice(0, 24); //set how many coins per page
+            const page = $(document.createElement('div'));
+
+            page.addClass('page');
+            page.attr('data-num', `${pagesCounter}`);
+            page.hide();
+
+            const storedData = JSON.parse(localStorage.getItem('coinsData'));
+
+
+            $.each(pageData, (_index,coinData) => {
+
+                const coin = card();
+                coin.attr('data-id', coinData.id);
+                coin.attr('data-symbol', coinData.symbol);
+                coin.attr('data-name', coinData.name);
+                coin.find('.card-header p').html(coinData.symbol.toUpperCase());
+                coin.find('.card-title').html(coinData.name);
+                coin.find('.info-container').hide();
+                if (storedData != null) {
+
+                    const storedCoin = $.grep(storedData, searchedCoin => searchedCoin.coinID === coinData.id);
+                    if (storedCoin.length > 0) {
+                        const tempRequest = new DataHandler(coinData.id)
+                        tempRequest.updateCoin(coin)
+                    }
+
+                }
+                coin.appendTo(page);
+            })
+
+            //append the first page as soon as it ready 
+            if (page.data('num') == 0) {
+                page.clone().prependTo('section');
+                $('section').find('.page').fadeIn('slow');
+                pagination().appendTo('section');
+                clearInterval(ticker);
+                if ($('.loader').css('display') === 'block') {
+                    $('.loader').css('width', "100%");
+                    $('.loader').fadeOut('slow', () => {
+                        $('.loader').css('width', "0%");
+                        $('.loader').show();
+
+                    })
+                }
+                
+            }
+
+            pagesCounter++;
+            page.appendTo(pagesContainer);
+
+            //stop the interval if there is no more data to create a coin
+            if (apiListData.length == 0) {
+                clearInterval(dataFlowValve);
+            }
+
+        })
+
+        return pagesContainer
+    }
+
+    async updateMarketData() {
+        const storedData = JSON.parse(localStorage.getItem('coinsData'));
+        const apiCoinData = await apiRequest(this.request);
+        const timestamp = $.now();
+        const filteredStoredData = $.grep(storedData, coinData => coinData.coinID != this.request);
+        const coinData = {
+
+            usd: apiCoinData.market_data.current_price.usd,
+            eur: apiCoinData.market_data.current_price.eur,
+            ils: apiCoinData.market_data.current_price.ils
+        }
+        filteredStoredData.push({ coinID: this.request, last_update: timestamp, market_data: coinData, image: apiCoinData.image.small });
+        localStorage.setItem('coinsData', JSON.stringify(filteredStoredData));
+        clearInterval(ticker);
+        if ($('.loader').css('display') === 'block') {
+            $('.loader').css('width', "100%");
+            $('.loader').fadeOut('slow', () => {
+                $('.loader').css('width', "0%");
+                $('.loader').show();
+
+            })
+        }
+
+
+    }
+
+    updateCoin(coin) {
+        const storedData = JSON.parse(localStorage.getItem('coinsData'));
+        const storedCoinArr = $.grep(storedData, coinData => coinData.coinID === this.request);
+        const storedCoin = storedCoinArr[0];
+        const coinListItem = coin.find('li');
+        coin.find('img').attr('src', storedCoin.image);
+        $.each(coinListItem, (_index,li) => {
+            switch (_index) {
+                case 0:
+                    $(li).html(`<h6>USD:</h6><p>${storedCoin.market_data.usd}$</p>`);
+                    break;
+                case 1:
+                    $(li).html(`<h6>EUR:</h6><p>${storedCoin.market_data.eur}€</p>`);
+                    break;
+                case 2:
+                    $(li).html(`<h6>ILS:</h6><p>${storedCoin.market_data.ils}₪</p>`);
+                    break;
+            }
+        }) 
+    }
+
+    async liveReport() {
+
+        const apiCoinData = await apiRequest(this.request);
+        clearInterval(ticker);
+        $('.loader').css('width', "0%");
+        return apiCoinData;
+
+    }
+    //function in case wants to try it the other way
+}
